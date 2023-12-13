53f479e7533e7ce0b4ce896f4ca06db54497be7c
ajay943 <96942300+ajay943@users.noreply.github.com>
2023-12-13 13:04:34 +0530

    13/12

--- lib/views/playercards.dart
+++ lib/views/playercards.dart
@@ -5,17 +5,50 @@ class TeamSelectionScreen extends StatefulWidget {
   _TeamSelectionScreenState createState() => _TeamSelectionScreenState();
 }
 
-class _TeamSelectionScreenState extends State<TeamSelectionScreen> with SingleTickerProviderStateMixin {
+class _TeamSelectionScreenState extends State<TeamSelectionScreen>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
   List<Player> players = [
-    Player(name: 'Player 1', position: 'Forward', skill: 'Attack', photo: 'assets/player1.jpg'),
-    Player(name: 'Player 2', position: 'Forward', skill: 'Attack', photo: 'assets/player2.jpg'),
-    Player(name: 'Player 3', position: 'Midfielder', skill: 'Midfield', photo: 'assets/player3.jpg'),
-    Player(name: 'Player 4', position: 'Midfielder', skill: 'Midfield', photo: 'assets/player4.jpg'),
-    Player(name: 'Player 5', position: 'Defender', skill: 'Defense', photo: 'assets/player5.jpg'),
-    Player(name: 'Player 6', position: 'Defender', skill: 'Defense', photo: 'assets/player6.jpg'),
-    Player(name: 'Player 7', position: 'Goalkeeper', skill: 'Goalkeeping', photo: 'assets/player7.jpg'),
-    Player(name: 'Player 8', position: 'Goalkeeper', skill: 'Goalkeeping', photo: 'assets/player8.jpg'),
+    Player(
+        name: 'Player 1',
+        position: 'Forward',
+        skill: 'Attack',
+        photo: 'assets/player1.jpg'),
+    Player(
+        name: 'Player 2',
+        position: 'Forward',
+        skill: 'Attack',
+        photo: 'assets/player2.jpg'),
+    Player(
+        name: 'Player 3',
+        position: 'Midfielder',
+        skill: 'Midfield',
+        photo: 'assets/player3.jpg'),
+    Player(
+        name: 'Player 4',
+        position: 'Midfielder',
+        skill: 'Midfield',
+        photo: 'assets/player4.jpg'),
+    Player(
+        name: 'Player 5',
+        position: 'Defender',
+        skill: 'Defense',
+        photo: 'assets/player5.jpg'),
+    Player(
+        name: 'Player 6',
+        position: 'Defender',
+        skill: 'Defense',
+        photo: 'assets/player6.jpg'),
+    Player(
+        name: 'Player 7',
+        position: 'Goalkeeper',
+        skill: 'Goalkeeping',
+        photo: 'assets/player7.jpg'),
+    Player(
+        name: 'Player 8',
+        position: 'Goalkeeper',
+        skill: 'Goalkeeping',
+        photo: 'assets/player8.jpg'),
   ];
 
   List<Player> selectedPlayers = [];
@@ -23,7 +56,8 @@ class _TeamSelectionScreenState extends State<TeamSelectionScreen> with SingleTi
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: _getUniqueSkills().length, vsync: this);
+    _tabController =
+        TabController(length: _getUniqueSkills().length, vsync: this);
   }
 
   List<String> _getUniqueSkills() {
@@ -39,68 +73,117 @@ class _TeamSelectionScreenState extends State<TeamSelectionScreen> with SingleTi
     return Scaffold(
       appBar: AppBar(
         title: Text('Team Selection'),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: _getUniqueSkills().map((skill) => Tab(text: skill)).toList(),
-        ),
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: _getUniqueSkills().map((skill) {
-          return Column(
-            children: [
-              Text('Select your $skill team:'),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _getPlayersBySkill(skill).length,
-                  itemBuilder: (context, index) {
-                    final player = _getPlayersBySkill(skill)[index];
-                    return ListTile(
-                      title: Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundImage: AssetImage(player.photo),
-                          ),
-                          SizedBox(width: 16.0),
-                          Text(player.name),
-                        ],
+      body: Column(
+        children: [
+          Container(
+            height: 200.0,
+            width: 700.0,
+            child: Card(
+              color: Colors.black,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Maximum of 10 players one team',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
                       ),
-                      subtitle: Text(player.position),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.remove),
-                            onPressed: () {
-                              setState(() {
-                                if (selectedPlayers.contains(player)) {
-                                  selectedPlayers.remove(player);
-                                }
-                              });
-                            },
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.add),
-                            onPressed: () {
-                              setState(() {
-                                if (!selectedPlayers.contains(player)) {
-                                  selectedPlayers.add(player);
-                                }
-                              });
-                            },
-                          ),
-                        ],
+                    ),
+                    SizedBox(
+                        height:
+                            16.0), // Add some space between text and progress bar
+                    LinearProgressIndicator(
+                      value: 0.3, // Set the progress value (e.g., 0.1 for 10%)
+                      backgroundColor: Colors
+                          .grey, // Set the background color of the progress bar
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                          Colors.blue), // Set the progress color
+                    ),
+                    SizedBox(
+                        height:
+                            8.0), // Add some space between progress bar and number
+                    Text(
+                      '11/11', // Display the number
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
                       ),
-                      tileColor: selectedPlayers.contains(player)
-                          ? Colors.blue.withOpacity(0.3)
-                          : null,
-                    );
-                  },
+                    ),
+                  ],
                 ),
               ),
-            ],
-          );
-        }).toList(),
+            ),
+          ),
+          TabBar(
+            controller: _tabController,
+            tabs: _getUniqueSkills().map((skill) => Tab(text: skill)).toList(),
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: _getUniqueSkills().map((skill) {
+                return Column(
+                  children: [
+                    Text('Select your $skill team:'),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _getPlayersBySkill(skill).length,
+                        itemBuilder: (context, index) {
+                          final player = _getPlayersBySkill(skill)[index];
+                          return ListTile(
+                            title: Row(
+                              children: [
+                                CircleAvatar(
+                                  backgroundImage: AssetImage(player.photo),
+                                ),
+                                SizedBox(width: 16.0),
+                                Text(player.name),
+                              ],
+                            ),
+                            subtitle: Text(player.position),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.remove),
+                                  onPressed: () {
+                                    setState(() {
+                                      if (selectedPlayers.contains(player)) {
+                                        selectedPlayers.remove(player);
+                                      }
+                                    });
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.add),
+                                  onPressed: () {
+                                    setState(() {
+                                      if (!selectedPlayers.contains(player)) {
+                                        selectedPlayers.add(player);
+                                      }
+                                    });
+                                  },
+                                ),
+                              ],
+                            ),
+                            tileColor: selectedPlayers.contains(player)
+                                ? Colors.blue.withOpacity(0.3)
+                                : null,
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: ElevatedButton(
         onPressed: () {
@@ -119,5 +202,9 @@ class Player {
   final String skill;
   final String photo;
 
-  Player({required this.name, required this.position, required this.skill, required this.photo});
+  Player(
+      {required this.name,
+      required this.position,
+      required this.skill,
+      required this.photo});
 }
